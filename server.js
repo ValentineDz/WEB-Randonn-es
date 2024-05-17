a2b57164318a085f75b983a05040a004d1e02067
Helies26 <helies26000@gmail.com>
2024-05-17 19:57:27 +0200

    .

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import path from "path";
 import multer from "multer";
 
 const databaseFile = "database.sqlite";
-const PORT = 3000;
+const PORT = 3000; 
+
 import * as indexRoute from "./routes/index.js";
 
 // Configuration de multer pour stocker les fichiers dans le dossier 'public/uploads'
@@ -23,25 +24,21 @@ const upload = multer({ storage: storage });
 
 function start(database) {
   const app = express();
-
   // Gestionnaire d'erreur de base
   // app.use((req, res, next) => {
   //   res.status(404).send("Désolé, cette page n'existe pas !");
-  // });
-
-  // Log la méthode et l'URL de chaque requête reçue par le serveur
-  app.use((request, response, next) => {
-    console.log(`${request.method} ${request.url}`);
-    next();
-  });
-
-  app.use((request, response, next) => {
-    request.context = request.context ?? {}; // Créez un objet context pour la requête si ce n'est pas déjà fait.
-    request.context.database = database;
-    next();
-  });
-
-  // Permet d'utiliser le CSS dans index.js par exemple
+  // }); ca fonctionne pas pour l'instant laisser de coté
+    // Log la méthode et l'URL de chaque requête reçue par le serveur
+    app.use((request, response, next) => {
+      console.log(`${request.method} ${request.url}`);
+      next();
+    });
+    app.use((request, response, next) => {
+      request.context = request.context ?? {}; // Créez un objet context pour la requête si ce n'est pas déjà fait.
+      request.context.database = database;
+      next();
+    });
+      // Permet d'utiliser le CSS dans index.js par exemple
   app.use(express.static("public"));
   app.use(express.static("public", { extensions: ["html"] }));
   app.use(express.json());
@@ -52,18 +49,15 @@ function start(database) {
   });
 
   app.get("/", indexRoute.get);
-
-  // Route pour Contribuer
-  app.get('/contribuer', (req, res) => {
+   // Route pour Contribuer
+   app.get('/contribuer', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/Contribuer.html'));
   });
-
-  // Route pour soumettre une nouvelle randonnée
-  app.post('/submit-randonnee', upload.single('photo'), (req, res) => {
-    const { nom, description, depart } = req.body;
-    const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
-
-    console.log("données reçues:", { nom, description, depart, imagePath });
+    // Route pour soumettre une nouvelle randonnée
+    app.post('/submit-randonnee', upload.single('photo'), (req, res) => {
+      const { nom, description, depart } = req.body;
+      const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
+      console.log("données reçues:", { nom, description, depart, imagePath });
     const sql = "INSERT INTO randonnees (nom, description, adresse_depart, image) VALUES (?, ?, ?, ?)";
     req.context.database.run(sql, [nom, description, depart, imagePath], function(err) {
       if (err) {
@@ -75,11 +69,11 @@ function start(database) {
       res.redirect(`/randonnee/${this.lastID}`);
     });
   });
-
-  // Démarrage du serveur
-  app.listen(PORT, () => {
+   // Démarrage du serveur
+   app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port http://localhost:${PORT}`);
   });
+
 }
 
 open({ filename: databaseFile, driver: sqlite3.Database })
